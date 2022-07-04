d113a5af0c0141d67a0d77d0e56aa17937ed2169
Николай <kostnn@list.ru>
2022-07-04 14:08:09 +0700

    2. Аттестация [#49777]

--- src/main/java/ru/job4j/stream/Tuple.java
+++ src/main/java/ru/job4j/stream/Tuple.java
@@ -12,21 +12,28 @@ public class Tuple {
         this.score = score;
     }
 
-    public static int compare (Tuple t1, Tuple t2){
-        if(t1.getScore() > t2.getScore())
+    public static int compare(Tuple t1, Tuple t2) {
+        if (t1.getScore() > t2.getScore()) {
             return 1;
+        }
         return -1;
     }
 
-    public Double getScore() { return score; }
+    public Double getScore() {
+        return score;
+    }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Tuple tuple = (Tuple) o;
-        return Double.compare(tuple.score, score) == 0 &&
-                Objects.equals(name, tuple.name);
+        return Double.compare(tuple.score, score) == 0
+                && Objects.equals(name, tuple.name);
     }
 
     @Override
